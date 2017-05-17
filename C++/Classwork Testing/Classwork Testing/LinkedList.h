64788a7888d4029b38a4301d2c2e076e0af2fc9e
AIE\S171842 <zackary.direen@hotmail.com>
2017-05-17 13:53:55 +1000

    no message

---
+++ C++/Classwork Testing/Classwork Testing/LinkedList.h
@@ -0,0 +1,24 @@
+#pragma once
+
+template<typename T>
+class ListNode {
+public:
+	ListNode* next;
+	ListNode* prev;
+	T value;
+};
+
+
+template<typename T>
+
+
+class List {
+
+public:
+
+
+private:
+	ListNode<T>* m_first;
+	ListNode<T>* m_last;
+
+};
